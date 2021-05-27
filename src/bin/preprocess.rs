cc35dd59ee377ea146987c3dc9af45053fb1fad2
Georgios Gkitsas <ggkitsas@yahoo.com>
2021-05-27 15:00:18 +0100

    saving work

--- src/bin/preprocess.rs
+++ src/bin/preprocess.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{self, BufReader, Read}};
+use std::{fs::{File, OpenOptions}, io::{self, BufReader, BufWriter, Read}};
 use ark_bls12_381::Bls12_381;
 use ark_ec::PairingEngine;
 use ark_serialize::{CanonicalDeserialize, CanonicalSerialize, SerializationError};
@@ -7,11 +7,15 @@ use pairing::{
     EncodedPoint,
 };
 use ark_poly_commit::kzg10::{Powers, VerifierKey};
+use powersoftau::{Accumulator, CONTRIBUTION_BYTE_SIZE, CheckForCorrectness, HashReader, UseCompression};
 
 type ArkG1Affine = <ark_ec::bls12::Bls12<ark_bls12_381::Parameters> as PairingEngine>::G1Affine;
 type ArkG2Affine = <ark_ec::bls12::Bls12<ark_bls12_381::Parameters> as PairingEngine>::G2Affine;
 
+const DOWNLOAD_URL: &str = "https://powersoftau-transcript.s3-us-west-2.amazonaws.com/88dc1dc6914e44568e8511eace177e6ecd9da9a9bd8f67e4c0c9f215b517db4d1d54a755d051978dbb85ef947918193c93cd4cf4c99c0dc5a767d4eeb10047a4";
+const POWERSOFTAU_DIGEST: &str = "88dc1dc6914e44568e8511eace177e6ecd9da9a9bd8f67e4c0c9f215b517db4d1d54a755d051978dbb85ef947918193c93cd4cf4c99c0dc5a767d4eeb10047a4";
 const POWERSOFTAU_FILE: &str = "powersoftau";
+const POWERSOFTAU_UNCOMPRESSED_FILE: &str = "powersoftau_uncompressed";
 const PARAMS_FILE: &str = "kzg_setup";
 const TAU_POWERS_LENGTH: usize = 1 << 21;
 const TAU_POWERS_G1_LENGTH: usize = (TAU_POWERS_LENGTH << 1) - 1;
@@ -20,47 +24,43 @@ struct PowersOfTau {
     tau_powers_g1: Vec<ArkG1Affine>,
     tau_powers_g2: Vec<ArkG2Affine>,
     alpha_tau_powers_g1: Vec<ArkG1Affine>,
-    beta_tau_powers_g1: Vec<ArkG1Affine>,
-    beta_g2: ArkG2Affine,
+    // beta_tau_powers_g1: Vec<ArkG1Affine>,
+    // beta_g2: ArkG2Affine,
 }
 
 pub fn download_parameters() -> Result<(), minreq::Error> {
-    const DOWNLOAD_URL: &str = "https://powersoftau-transcript.s3-us-west-2.amazonaws.com/88dc1dc6914e44568e8511eace177e6ecd9da9a9bd8f67e4c0c9f215b517db4d1d54a755d051978dbb85ef947918193c93cd4cf4c99c0dc5a767d4eeb10047a4";
-
-    let fetch_params = |expected_hash: &str| -> Result<(), minreq::Error> {
+    // let fetch_params = |expected_hash: &str| -> Result<(), minreq::Error> {
         use std::io::Write;
 
         let part_1 = minreq::get(DOWNLOAD_URL).send()?;
 
-        // TODO
-        // // Verify parameter file hash.
-        // let hash = blake2b_simd::State::new()
-        //     .update(part_1.as_bytes())
-        //     .finalize()
-        //     .to_hex();
-        // if &hash != expected_hash {
-        //     return Err(io::Error::new(
-        //         io::ErrorKind::InvalidData,
-        //         format!(
-        //             "{} failed validation (expected: {}, actual: {}, fetched {} bytes)",
-        //             exp,
-        //             expected_hash,
-        //             hash,
-        //             part_1.as_bytes().len()
-        //         ),
-        //     )
-        //     .into());
-        // }
+        // Verify parameter file hash.
+        let hash = blake2b_simd::State::new()
+            .update(part_1.as_bytes())
+            .finalize()
+            .to_hex();
+        if &hash != POWERSOFTAU_DIGEST {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!(
+                    "failed validation (expected: {}, actual: {}, fetched {} bytes)",
+                    POWERSOFTAU_DIGEST,
+                    hash,
+                    part_1.as_bytes().len()
+                ),
+            )
+            .into());
+        }
 
         // Write parameter file.
         let mut f = File::create(POWERSOFTAU_FILE)?;
         f.write_all(part_1.as_bytes())?;
         Ok(())
-    };
+    // };
 
-    fetch_params("")?;
+    // fetch_params(POWERSOFTAU_DIGEST)?;
 
-    Ok(())
+    // Ok(())
 }
 
 fn read_g1(reader: &mut BufReader<File>) -> Result<ArkG1Affine, SerializationError> {
@@ -74,7 +74,7 @@ fn read_g1(reader: &mut BufReader<File>) -> Result<ArkG1Affine, SerializationErr
     repr_bytes_vec[000..=047].reverse();
     repr_bytes_vec[048..=095].reverse();
 
-    let repr_new = ArkG1Affine::deserialize_uncompressed(&repr_bytes_vec[..]);
+    let repr_new = ArkG1Affine::deserialize_unchecked(&repr_bytes_vec[..]);
     repr_new
 }
 
@@ -100,11 +100,46 @@ fn read_g2(reader: &mut BufReader<File>) -> Result<ArkG2Affine, SerializationErr
     repr_bytes_vec[096..=143].reverse();
     repr_bytes_vec[144..=191].reverse();
 
-    let repr_new = ArkG2Affine::deserialize_uncompressed(&repr_bytes_vec[..]);
+    let repr_new = ArkG2Affine::deserialize_unchecked(&repr_bytes_vec[..]);
     repr_new
 }
 
-fn load_powersoftau_accum() -> io::Result<PowersOfTau> {
+fn powersoftau_uncompress() {
+    let response_reader = OpenOptions::new()
+                            .read(true)
+                            .open(POWERSOFTAU_FILE).expect(&format!("unable open `{}` in this directory", POWERSOFTAU_FILE));
+
+    {
+        let metadata = response_reader.metadata().expect(&format!("unable to get filesystem metadata for `{}`", POWERSOFTAU_FILE));
+        if metadata.len() != (CONTRIBUTION_BYTE_SIZE as u64) {
+            panic!("The size of `{}` should be {}, but it's {}, so something isn't right.", POWERSOFTAU_FILE, CONTRIBUTION_BYTE_SIZE, metadata.len());
+        }
+    }
+
+    let response_reader = BufReader::new(response_reader);
+    let mut response_reader = HashReader::new(response_reader);
+
+    println!("Started deserializing compressed Powers of Tau...");
+    // Load the response's accumulator
+    let new_accumulator = Accumulator::deserialize(&mut response_reader, UseCompression::Yes, CheckForCorrectness::No)
+                                                  .expect("wasn't able to deserialize the response file's accumulator");
+
+    println!("Done deserializing.");
+    let writer = OpenOptions::new()
+    .read(false)
+    .write(true)
+    .create_new(true)
+    .open(POWERSOFTAU_UNCOMPRESSED_FILE).expect(&format!("unable to create `{}`", POWERSOFTAU_UNCOMPRESSED_FILE).as_ref());
+
+    let mut writer = BufWriter::new(writer);
+    println!("Started serializing unompressed Powers of Tau...");
+    new_accumulator.serialize(&mut writer, UseCompression::No)
+                    .expect("wasn't able to deserialize the response file's accumulator");
+    println!("Done serializing.");
+
+}
+
+fn load_powersoftau_accumulator() -> io::Result<PowersOfTau> {
     let f = match File::open(POWERSOFTAU_FILE) {
         Ok(f) => f,
         Err(e) => {
@@ -128,29 +163,32 @@ fn load_powersoftau_accum() -> io::Result<PowersOfTau> {
         alpha_tau_powers_g1.push(read_g1(f).unwrap());
     }
 
-    let mut beta_tau_powers_g1 = Vec::with_capacity(TAU_POWERS_LENGTH);
-    for _ in 0..TAU_POWERS_LENGTH {
-        beta_tau_powers_g1.push(read_g1(f).unwrap());
-    }
+    // let mut beta_tau_powers_g1 = Vec::with_capacity(TAU_POWERS_LENGTH);
+    // for _ in 0..TAU_POWERS_LENGTH {
+    //     beta_tau_powers_g1.push(read_g1(f).unwrap());
+    // }
 
-    let beta_g2 = read_g2(f).unwrap(); //?;
+    // let beta_g2 = read_g2(f).unwrap();
 
     Ok(PowersOfTau {
         tau_powers_g1: tau_powers_g1,
         tau_powers_g2: tau_powers_g2,
         alpha_tau_powers_g1: alpha_tau_powers_g1,
-        beta_tau_powers_g1: beta_tau_powers_g1,
-        beta_g2: beta_g2
+        // beta_tau_powers_g1: beta_tau_powers_g1,
+        // beta_g2: beta_g2
     })
 }
 
 fn main() {
-    // download_parameters().unwrap();
+    download_parameters().unwrap();
     println!("Downloaded Powers of Tau");
 
-    let params = load_powersoftau_accum().unwrap();
+    powersoftau_uncompress();
+
+    let params = load_powersoftau_accumulator().unwrap();
     println!("Loaded Powers of Tau");
 
+    println!("Preparing KZG parameters");
     let powersoftau = Powers::<Bls12_381> {
         powers_of_g: ark_std::borrow::Cow::Owned(params.tau_powers_g1.to_vec()),
         powers_of_gamma_g: ark_std::borrow::Cow::Owned(params.alpha_tau_powers_g1.to_vec()),
@@ -165,6 +203,7 @@ fn main() {
         prepared_beta_h: params.tau_powers_g2[1].into(),
     };
 
+    println!("Serializing KZG parameters...");
     let buffer = File::create(PARAMS_FILE).unwrap();
     for g in powersoftau.powers_of_g.iter() {
         g.serialize_uncompressed(&buffer).unwrap();
@@ -173,4 +212,6 @@ fn main() {
         g.serialize_uncompressed(&buffer).unwrap();
     }
     vk.serialize_uncompressed(buffer).unwrap();
+
+    println!("Done serializing. KZG parameters are stored in {}", PARAMS_FILE);
 }
